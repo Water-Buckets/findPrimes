c133b276e01b62041f4966b912ae55f8316e35ad
WaterBucket <lizirui3724@gmail.com>
2023-07-02 19:11:33 +0800

    Remove commented code in prime number generator
    Cleaned up the primes.cpp file by removing several commented out sections of code. These sections do not contribute to the code's functionality but clutter the file and make it less readable. The parts removed were alternate implementations of various prime number generators (including the Eratosthenes, Sundaram, Atkin, Incremental, and Wheel sieves) that were not being used in the final version.

--- primes.cpp
+++ primes.cpp
@@ -17,22 +17,6 @@ std::vector<long long> trialDivision(long long n) {
     return primes;
 }
 
-//std::vector<long long> eratosthenesSieve(long long n) {
-//    std::vector<bool> isPrime(n + 1, true);
-//    std::vector<long long> primes;
-//    isPrime[0] = isPrime[1] = false;
-//    for (long long i = 2; i <= n; i++) {
-//        if (isPrime[i]) {
-//            primes.push_back(i);
-//            if (i * i <= n) {
-//                for (long long j = i * i; j <= n; j += i) {
-//                    isPrime[j] = false;
-//                }
-//            }
-//        }
-//    }
-//    return primes;
-//}
 std::vector<long long> eratosthenesSieve(long long n) {
     std::vector<bool> isPrime((n+2) / 2, true);
     std::vector<long long> primes;
@@ -64,18 +48,6 @@ std::vector<long long> eulerSieve(long long n) {
     return primes;
 }
 
-//std::vector<long long> sundaramSieve(long long n) {
-//    long long nNew = (n - 2) / 2;
-//    std::vector<bool> marked(nNew + 1, false);
-//    std::vector<long long> primes;
-//    for (long long i = 1; i <= nNew; i++)
-//        for (long long j = i; (i + j + 2 * i * j) <= nNew; j++)
-//            marked[i + j + 2 * i * j] = true;
-//    if (n > 2) primes.push_back(2);
-//    for (long long i = 1; i <= nNew; i++)
-//        if (!marked[i]) primes.push_back(2 * i + 1);
-//    return primes;
-//}
 std::vector<long long> sundaramSieve(long long n) {
     long long nNew = n / 2;
     std::vector<bool> marked(nNew + 1, false);
@@ -89,31 +61,6 @@ std::vector<long long> sundaramSieve(long long n) {
     return primes;
 }
 
-//std::vector<long long> atkinSieve(long long n) {
-//    std::vector<bool> sieve(n + 1, false);
-//    std::vector<long long> primes;
-//    for (long long x = 1; x * x <= n; x++) {
-//        for (long long y = 1; y * y <= n; y++) {
-//            long long w = 4*x*x + y*y;
-//            if (w <= n && (w % 12 == 1 || w % 12 == 5)) sieve[w] = !sieve[w];
-//            w = 3*x*x + y*y;
-//            if (w <= n && w % 12 == 7) sieve[w] = !sieve[w];
-//            w = 3*x*x - y*y;
-//            if (x > y && w <= n && w % 12 == 11) sieve[w] = !sieve[w];
-//        }
-//    }
-//    for (long long r = 5; r * r <= n; r++) {
-//        if (sieve[r]) {
-//            for (long long i = r * r; i <= n; i += r * r)
-//                sieve[i] = false;
-//        }
-//    }
-//    if (n > 2) primes.push_back(2);
-//    if (n > 3) primes.push_back(3);
-//    for (long long a = 5; a <= n; a+=2)
-//        if (sieve[a]) primes.push_back(a);
-//    return primes;
-//}
 std::vector<long long> atkinSieve(long long n) {
     std::vector<bool> isPrime(n + 1, false);
     std::vector<long long> primes;
@@ -144,18 +91,7 @@ std::vector<long long> atkinSieve(long long n) {
     return primes;
 }
 
-//std::vector<long long> incrementalSieve(long long n) {
-//    std::vector<long long> primes;
-//    std::vector<bool> sieve(n + 1, true);
-//    for (long long p = 2; p <= n; p++) {
-//        if (sieve[p]) {
-//            primes.push_back(p);
-//            for (long long i = p * p; i <= n; i += p)
-//                sieve[i] = false;
-//        }
-//    }
-//    return primes;
-//}
+
 std::vector<long long> incrementalSieve(long long n) {
     std::vector<bool> sieve(n + 1, true);
     std::vector<long long> primes;
@@ -170,18 +106,6 @@ std::vector<long long> incrementalSieve(long long n) {
     return primes;
 }
 
-//std::vector<long long> wheelSieve(long long n) {
-//    std::vector<long long> primes;
-//    std::vector<bool> sieve(n + 1, true);
-//    for (long long p = 2; p <= n; p++) {
-//        if (sieve[p]) {
-//            primes.push_back(p);
-//            for (long long i = p * p; i <= n; i += p * 2)
-//                sieve[i] = false;
-//        }
-//    }
-//    return primes;
-//}
 
 std::vector<long long> wheelSieve(long long n) {
     std::vector<long long> primes;
